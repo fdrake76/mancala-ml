2df28dc0e290c5c06fb48d92204ca5cba61ad8c8
Fred Drake <fred.drake@gmail.com>
2018-10-24 08:21:58 -0400

    Merge fixes

--- src/main/java/com/freddrake/mancala/mancalaml/engine/AbstractGamingEngine.java
+++ src/main/java/com/freddrake/mancala/mancalaml/engine/AbstractGamingEngine.java
@@ -1,21 +1,23 @@
 package com.freddrake.mancala.mancalaml.engine;
 
 import com.freddrake.mancala.mancalaml.GameBoard;
+import com.freddrake.mancala.mancalaml.Player;
 
 public abstract class AbstractGamingEngine implements GamingEngine {
+	protected Player player;
 
 	@Override
-	public void onAfterMove(GameBoard gameBoard) {}
+    public boolean executeMove(GameBoard gameBoard) {
+	    boolean playAgain = true;
+	    while (playAgain) {
+	        if (gameBoard.isGameOver(player)) {
+	            // We want to play but game is over.
+                return true;
+            }
+	        int move = chooseMove(gameBoard);
+	        playAgain = gameBoard.executeMove(player, move);
+        }
 
-	@Override
-	public void onAfterGame(GameBoard gameBoard) {}
-
-	@Override
-    public void onBeforeSession() {}
-
-	@Override
-    public void onAfterSession() {}
-
-	@Override
-	public void saveNetwork() {}
+        return false;
+    }
 }
--- src/main/java/com/freddrake/mancala/mancalaml/spring/KeyboardPlayerComponent.java
+++ src/main/java/com/freddrake/mancala/mancalaml/spring/KeyboardPlayerComponent.java
@@ -1,11 +1,7 @@
 package com.freddrake.mancala.mancalaml.spring;
 
 
-import com.freddrake.mancala.mancalaml.GameSession;
 import com.freddrake.mancala.mancalaml.engine.KeyboardInputEngine;
-import com.freddrake.mancala.mancalaml.engine.QLearningEngine;
-import com.freddrake.mancala.mancalaml.engine.RandomEngine;
-import com.freddrake.mancala.mancalaml.stats.Statistician;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,19 +14,19 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class KeyboardPlayerComponent implements CommandLineRunner {
     private static final Logger log = LoggerFactory.getLogger(KeyboardPlayerComponent.class);
-    private QLearningEngine nonTrainingEngine;
+//    private QLearningEngine nonTrainingEngine;
     private KeyboardInputEngine keyboardInputEngine;
 
     @Override
     public void run(String... args) {
-        GameSession session =  GameSession.builder()
-                .player1Engine(nonTrainingEngine)
-                .player2Engine(keyboardInputEngine)
-                .trainingGames(1)
-                .build();
-
-        log.info("Playing a game against the machine learning opponent.  You are player two.");
-
-        session.train();
+//        GameSession session =  GameSession.builder()
+//                .player1Engine(nonTrainingEngine)
+//                .player2Engine(keyboardInputEngine)
+//                .trainingGames(1)
+//                .build();
+//
+//        log.info("Playing a game against the machine learning opponent.  You are player two.");
+//
+//        session.train();
     }
 }
